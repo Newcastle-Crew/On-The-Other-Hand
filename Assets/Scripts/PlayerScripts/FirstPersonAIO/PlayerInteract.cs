a885b9be97605451952ce3cc4f29929df1aea030
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-07-21 03:09:01 +0100

    Guy in a chair, big rat + healing mechanic.

--- Assets/Scripts/PlayerScripts/FirstPersonAIO/PlayerInteract.cs
+++ Assets/Scripts/PlayerScripts/FirstPersonAIO/PlayerInteract.cs
@@ -12,25 +12,15 @@ public class PlayerInteract : MonoBehaviour
     public IInteractable Interactable { get; set; }
     #endregion
     
-    #region Health Variables
-
-    public int maxHealth = 600;
-    public int currentHealth;
-    private int DamageOverTime = 1;
-    public HealthBar healthBar;
-
-    #endregion
-    
     private void Start() /// Start is called before the first frame update
     {        
-        currentHealth = maxHealth; // Starts the game and has the player at maximum health.
-        healthBar.SetMaxHealth(maxHealth);
-        StartCoroutine(BleedOut());
+        
     }
 
     private void Update()
     {   
-        if (dialogueUI.IsOpen) return; // Stops the player from spamming extra dialogue boxes while the dialogue box is appearing.
+        if (dialogueUI.IsOpen) // Stops the player from spamming extra dialogue boxes while the dialogue box is appearing.
+        return; 
 
         if (Input.GetKeyDown(KeyCode.E)) // If the player presses E and the object can be interacted with, interact with it.
         {
@@ -38,27 +28,4 @@ public class PlayerInteract : MonoBehaviour
         }
     }
 
-    IEnumerator BleedOut()
-    {
-        float lastDecrementTime = 0f;
-        float delayBetweenDecrements = 1f;
-
-        while (currentHealth > 0)
-  {
-        if (Time.time - lastDecrementTime > delayBetweenDecrements)
-        {
-            currentHealth -= 1;
-            healthBar.SetHealth(currentHealth);
-            lastDecrementTime += delayBetweenDecrements;
-        }
-        yield return null;
-        }
-    }
-
-    void TakeDamage (int damage)
-    {
-        currentHealth -= damage; // Reduces health number by the number of damage dealt.
-
-        healthBar.SetHealth(currentHealth);
-    }
 }
--- Assets/Scripts/PlayerScripts/Health/HealthBar.cs
+++ Assets/Scripts/PlayerScripts/Health/HealthBar.cs
@@ -19,5 +19,4 @@ public class HealthBar : MonoBehaviour
         slider.value = health;
     }
     
-
 }
