74467799e0f2ef55e0ba4d2bd22bd713b8b75419
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-08-09 22:25:03 +0100

    Halfway through dialogue choices

--- Assets/Scripts/DialogueScripts/DialogueObject.cs
+++ Assets/Scripts/DialogueScripts/DialogueObject.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
+
 public class DialogueObject : ScriptableObject // Template for dialogue objects to draw fields from.
 {
     [SerializeField] [TextArea] private string[] dialogue;
+    [SerializeField] private Response[] responses;
 
     public string[] Dialogue => dialogue; // Stops any outside code from overwriting the desired dialogue.
+
+    public Response[] Responses => responses;
 }
