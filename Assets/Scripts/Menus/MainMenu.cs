455f1da53e25edef77f9dd302827f2765ccf559b
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2022-07-23 22:33:47 +0100

    Main Menu & Pause Menu
    The main game scene pauses when entered, but otherwise good. Maybe there could be an image with text to read at the start?
    
    Also found a bug with the sensitivity - see discord for details.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,18 +1,29 @@
+﻿#region 'Using' information.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#endregion
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    /*
+        Click on the buttons, then add one of the two methods below via the 'OnClicked' part that handles events in the inspector.         
+    */
+
+    private void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); // Loads the game.
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
+
+    public void PlayGame()
+    { SceneManager.LoadScene("TestScene"); } // Loads the game.
 
     public void QuitGame()
     {
-        Debug.Log("QUIT!");
+        Debug.Log("QUIT!"); // Prints 'QUIT' if testing this in the editor.
         Application.Quit(); // Closes the game.
     }
 }
---
+++ Assets/Scripts/Menus/OpenLinks.cs
@@ -0,0 +1,36 @@
+#region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class OpenLinks : MonoBehaviour
+{
+    // credits for the team 
+
+    public void ScottCredit()
+    { Application.OpenURL("https://itch.io/profile/thefancyevil"); }
+
+    public void LawtonCredit()
+    { Application.OpenURL("https://www.instagram.com/lami_made_a_thing/"); }
+
+    public void JzuccCredit()
+    { Application.OpenURL("https://jzucc12.itch.io"); }
+
+    public void LeonCredit()
+    { Application.OpenURL("https://dxrkest.itch.io"); }
+
+    public void WestyCredit()
+    { Application.OpenURL("https://westydesign.itch.io/"); }
+
+    public void TrolledCredit()
+    { Application.OpenURL("https://trolledwoods.itch.io/"); }
+
+    // credits for stuff we use
+
+    public void FontCredit()
+    { Application.OpenURL("https://www.dafont.com"); }
+
+    public void SoundCredit()
+    { Application.OpenURL("https://www.zapsplat.com"); }
+}
