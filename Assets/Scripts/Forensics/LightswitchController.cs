22485746159d2a20fa2016e4ed2230735642fa6e
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2023-08-15 22:43:06 +0100

    Lightswitch updated
    Makes puzzle #1 a lot more accessible.

--- Assets/Scripts/Forensics/LightswitchController.cs
+++ Assets/Scripts/Forensics/LightswitchController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 #endregion
 
 public class LightswitchController : MonoBehaviour
@@ -10,19 +11,36 @@ public class LightswitchController : MonoBehaviour
     [SerializeField] private bool isLightOn;
     [SerializeField] private UnityEvent lightOnEvent;
     [SerializeField] private UnityEvent lightOffEvent;
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Material lightOnMaterial;
+    [SerializeField] private Material lightOffMaterial;
+
+    private void Start()
+    {
+        // Set initial material based on the starting state
+        UpdateMaterial();
+    }
 
     public void InteractSwitch()
     {
-        if(!isLightOn)
+        isLightOn = !isLightOn; // Toggle the light state
+
+        if (isLightOn)
         {
-            isLightOn = true;
             lightOnEvent.Invoke();
         }
         else
         {
-            isLightOn = false;
             lightOffEvent.Invoke();
         }
 
+        UpdateMaterial(); // Update the material based on the new state
+    }
+
+
+    private void UpdateMaterial()
+    {
+        // Assign the appropriate material based on the light state
+        meshRenderer.material = isLightOn ? lightOnMaterial : lightOffMaterial;
     }
 }
